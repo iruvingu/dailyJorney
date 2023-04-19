4f5559ff5491e53f499c393248c30662324bf89e
iruvingu <rfloresg1002@alumno.ipn.mx>
2023-04-19 13:27:09 -0600

    Update app to get root route at the end of the middlewares

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 ///jshint esversion:6
 
 const express = require("express");
-const ejs = require("ejs");
 
 const { router: homeRouter } = require("./routes/homeRouter.js");
 const { router: aboutRouter } = require("./routes/aboutRouter.js");
@@ -15,9 +14,9 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.use("/", homeRouter);
 app.use("/about", aboutRouter);
 app.use("/contact", contactRouter);
+app.use("/", homeRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
